8a4ffb6a94b625d8f26c622f5ea22be026df1d13
Abdurrehman006 <Abdurrehman4007@gmail.com>
2024-03-03 03:02:35 +0500

    initial commit with my changes

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,24 @@
+import React, {useContext} from "react";
+import "./Footer.scss";
+import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Footer() {
+  const {isDark} = useContext(StyleContext);
+  return (
+    <Fade bottom duration={1000} distance="5px">
+      <div className="footer-div">
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("Made with ❤️ by Sami Ullah")}
+        </p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          Theme by{" "}
+          <a href="https://github.com/Sami1ullah123">
+           Sami Ullah
+          </a>
+        </p>
+      </div>
+    </Fade>
+  );
+}
